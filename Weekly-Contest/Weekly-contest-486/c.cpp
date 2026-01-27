b47656471fbbc38f965b2f59aa58689bc0901e97
Mayank <mayankjha8274@gmail.com>
2026-01-27 17:25:04 +0530

    problem c updated

--- Weekly-Contest/Weekly-contest-486/c.cpp
+++ Weekly-Contest/Weekly-contest-486/c.cpp
@@ -1,12 +1,12 @@
 class Solution {
 public:
-    vector<int> bfs( int start , vector<vector<int>>& adj , int n ) {
+    vector<int> bfs( int n , vector<vector<int>>& adj , int node ) {
 
-        vector<int> dis( n , -1 ) ;
+        vector<int> dis(n , INT_MAX) ;
+        dis[node] = 0 ;
+
         queue<int> q ;
-
-        q.push(start) ;
-        dis[start] = 0 ;
+        q.push(node) ;
 
         while ( !q.empty() ) {
 
@@ -14,52 +14,87 @@ public:
 
             for ( auto v : adj[it] ) {
 
-                if ( dis[v] == -1 ) {
+                if ( dis[v] > dis[it] + 1 ) {
 
                     dis[v] = dis[it] + 1 ;
                     q.push(v) ;
-                    
+
                 }
             }
         }
 
         return dis ;
-        
+
+        // T.C = O( N + E )
+        // S.C = O( N + E ) --> bcz of adj list construction
+
     }
 
-    
-    int specialNodes(int n, vector<vector<int>>& edges, int x, int y, int z) {
+    vector<int> dijsktra( int n , vector<vector<int>>& adj , int node ) {
+      
+        vector<int> dis(n , INT_MAX) ;
+        dis[node] = 0 ;
 
+        priority_queue< pair<int,int> , vector<pair<int,int>> , greater<pair<int,int>> > pq ;
+
+        pq.push({ 0 , node }) ;
+
+        while ( !pq.empty() ) {
+
+            auto it = pq.top() ; pq.pop() ;
+
+            int distance = it.first ;
+            int curr = it.second ;
+
+            for ( auto v : adj[curr] ) {
+
+                if ( dis[v] > dis[curr] + 1 ) {
+
+                    dis[v] = dis[curr] + 1 ;
+                    pq.push({ dis[v] , v }) ;
+
+                }
+            }
+        }
+
+        return dis ;
+
+        // T.C = O((V + E) log n) = O(ElogV)​
+        // S.C = O(V + E)
+
+    }
+
+    // When all edge weights are equal (1), BFS is optimal and faster than Dijkstra.
+
+    // we can implement dijstra using set also but priority queue wala method is fast
+
+    int specialNodes(int n, vector<vector<int>>& edges, int x, int y, int z) {
+        
         vector<vector<int>> adj(n) ;
 
         for ( auto it : edges ) {
 
             adj[it[0]].push_back(it[1]) ;
             adj[it[1]].push_back(it[0]) ;
-            
+
         }
 
-        vector<int> dx = bfs( x , adj , n ) ;
-        vector<int> dy = bfs( y , adj , n ) ;
-        vector<int> dz = bfs( z , adj , n ) ;
+        vector<int> dx = dijsktra( n , adj , x ) ;
+        vector<int> dy = dijsktra( n , adj , y ) ;
+        vector<int> dz = dijsktra( n , adj , z ) ;
 
         int cnt = 0 ;
 
         for ( int i = 0 ; i < n ; i++ ) {
 
-            vector<int> temp = { dx[i] , dy[i] , dz[i] } ;
+            vector<int> ans = { dx[i] , dy[i] , dz[i] } ;
+            sort( ans.begin() , ans.end() ) ;
 
-            sort( temp.begin() , temp.end() ) ;
+            if ( 1LL * ans[0] * ans[0] + 1LL * ans[1] * ans[1] == 1LL * ans[2] * ans[2] ) cnt++ ;
 
-            int a = temp[0] ;
-            int b = temp[1] ;
-            int c = temp[2] ;
-
-            if ( 1LL * a * a + 1LL * b * b == 1LL * c * c ) cnt++ ;
-            
         }
 
         return cnt ;
-        
+
     }
 };
