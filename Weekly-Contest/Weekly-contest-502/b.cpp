e66fc98ecb5cc7627aa286f8934a7197fc9a12d2
Mayank <mayankjha8274@gmail.com>
2026-05-20 20:49:08 +0530

    weekly contest 502

---
+++ Weekly-Contest/Weekly-contest-502/b.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int countKthRoots(int l, int r, int k) {
+
+        // y = x^k
+        // l <= x^k <= r
+        // krootl <= x <= krootr
+
+        long long left = ceil( pow( l , 1.0 / k ) - 1e-9 )  ;
+        long long right = floor( pow( r , 1.0 / k ) + 1e-9 ) ;
+
+        return max( 0LL , right - left + 1 ) ;
+        
+    }
+};
